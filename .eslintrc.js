134f082d0a1c36dbfe02df338c805840aee7cf64
Darlan H <darlanhermessjb@hotmail.com>
2022-09-21 22:28:54 -0300

    chore: config eslint with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
