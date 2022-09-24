5990e489cf32ac0295792ebe8a79f9f2b6da52e2
Darlan H <darlanhermessjb@hotmail.com>
2022-09-24 17:07:25 -0300

    feat(frontend): new container component

---
+++ frontend/src/components/Container/index.tsx
@@ -0,0 +1,15 @@
+import clsx from 'classnames';
+
+import styles from './styles.module.css';
+
+type ContainerProps = React.HTMLAttributes<HTMLDivElement>;
+
+const Container: React.FC<ContainerProps> = ({ children, className, ...rest }) => {
+  return (
+    <div {...rest} className={clsx(styles.container, className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
+import Container from '../../components/Container';
 import { VStack } from '../../components/Stack';
 import Table from '../../components/Table';
 import styles from './styles.module.css';
@@ -8,61 +9,63 @@ const Home: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <VStack spacing={2} className={styles.container}>
-      <h3 className={styles.title}>Dispositivos eletrônicos</h3>
-      <div className={styles.buttonsContainer}>
-        <div className={styles.actionButtons}>
-          <Button disabled>Editar</Button>
-          <Button variant="error" disabled>
-            Excluir
-          </Button>
+    <Container className={styles.container}>
+      <VStack spacing={2}>
+        <h3 className={styles.title}>Dispositivos eletrônicos</h3>
+        <div className={styles.buttonsContainer}>
+          <div className={styles.actionButtons}>
+            <Button disabled>Editar</Button>
+            <Button variant="error" disabled>
+              Excluir
+            </Button>
+          </div>
+          <div style={{ flex: 1 }} />
+          <div>
+            <Button variant="success" onClick={() => navigate('/new')}>
+              Adicionar
+            </Button>
+          </div>
         </div>
-        <div style={{ flex: 1 }} />
-        <div>
-          <Button variant="success" onClick={() => navigate('/new')}>
-            Adicionar
-          </Button>
-        </div>
-      </div>
-      <Table.Container>
-        <Table>
-          <Table.Head>
-            <Table.Row>
-              <Table.Th>Nome</Table.Th>
-              <Table.Th>Serial</Table.Th>
-              <Table.Th>Mac Address</Table.Th>
-              <Table.Th>Tipo</Table.Th>
-            </Table.Row>
-          </Table.Head>
-          <Table.Body>
-            <Table.Row>
-              <Table.Td>Câmera da sala</Table.Td>
-              <Table.Td>asjkdfj192839asda</Table.Td>
-              <Table.Td>09-A8-34-23-12</Table.Td>
-              <Table.Td>Câmera</Table.Td>
-            </Table.Row>
-            <Table.Row>
-              <Table.Td>Câmera da sala</Table.Td>
-              <Table.Td>asjkdfj192839asda</Table.Td>
-              <Table.Td>09-A8-34-23-12</Table.Td>
-              <Table.Td>Câmera</Table.Td>
-            </Table.Row>
-            <Table.Row>
-              <Table.Td>Câmera da sala</Table.Td>
-              <Table.Td>asjkdfj192839asda</Table.Td>
-              <Table.Td>09-A8-34-23-12</Table.Td>
-              <Table.Td>Câmera</Table.Td>
-            </Table.Row>
-            <Table.Row>
-              <Table.Td>Câmera da sala</Table.Td>
-              <Table.Td>asjkdfj192839asda</Table.Td>
-              <Table.Td>09-A8-34-23-12</Table.Td>
-              <Table.Td>Câmera</Table.Td>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-      </Table.Container>
-    </VStack>
+        <Table.Container>
+          <Table>
+            <Table.Head>
+              <Table.Row>
+                <Table.Th>Nome</Table.Th>
+                <Table.Th>Serial</Table.Th>
+                <Table.Th>Mac Address</Table.Th>
+                <Table.Th>Tipo</Table.Th>
+              </Table.Row>
+            </Table.Head>
+            <Table.Body>
+              <Table.Row>
+                <Table.Td>Câmera da sala</Table.Td>
+                <Table.Td>asjkdfj192839asda</Table.Td>
+                <Table.Td>09-A8-34-23-12</Table.Td>
+                <Table.Td>Câmera</Table.Td>
+              </Table.Row>
+              <Table.Row>
+                <Table.Td>Câmera da sala</Table.Td>
+                <Table.Td>asjkdfj192839asda</Table.Td>
+                <Table.Td>09-A8-34-23-12</Table.Td>
+                <Table.Td>Câmera</Table.Td>
+              </Table.Row>
+              <Table.Row>
+                <Table.Td>Câmera da sala</Table.Td>
+                <Table.Td>asjkdfj192839asda</Table.Td>
+                <Table.Td>09-A8-34-23-12</Table.Td>
+                <Table.Td>Câmera</Table.Td>
+              </Table.Row>
+              <Table.Row>
+                <Table.Td>Câmera da sala</Table.Td>
+                <Table.Td>asjkdfj192839asda</Table.Td>
+                <Table.Td>09-A8-34-23-12</Table.Td>
+                <Table.Td>Câmera</Table.Td>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Table.Container>
+      </VStack>
+    </Container>
   );
 };
 
